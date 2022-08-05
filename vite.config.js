16625ebe706712505f3e0d55bef0ff4af86cdc8f
Ian <iansedano@gmail.com>
2022-08-05 16:57:11 +0200

    initial config with aliases

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	resolve: {
+		alias: {
+			// these are the aliases and paths to them
+			$components: path.resolve('./src/components')
+		}
+	}
 };
 
 export default config;
