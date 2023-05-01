7648b70803e5277996b27d3455b01a7677ec480e
Ian Currie <iansedano@gmail.com>
2023-05-01 18:19:16 +0200

    starting to upgrade to latest version of sveltekit

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,90 +1,91 @@
 <script>
-	import DarkModeToggle from '$components/DarkModeToggle.svelte';
+  import DarkModeToggle from '$components/DarkModeToggle.svelte';
 
-	import gsap from 'gsap';
-	import { onMount } from 'svelte';
+  import gsap from 'gsap';
+  import { onMount } from 'svelte';
 
-	// gsap.defaults({ ease: 'slow(0.9, 0.7, false)', duration: 5 });
+  // gsap.defaults({ ease: 'slow(0.9, 0.7, false)', duration: 5 });
 
-	onMount(() => {
-		const headerDiv = document.querySelector('header');
-		const headerHeight = headerDiv.clientHeight;
+  onMount(() => {
+    const headerDiv = document.querySelector('header');
+    if (!headerDiv) return;
+    const headerHeight = headerDiv.clientHeight;
 
-		let prevScrollPos = window.pageYOffset;
+    let prevScrollPos = window.pageYOffset;
 
-		window.onscroll = function () {
-			const currentScrollPos = window.pageYOffset;
-			const down = currentScrollPos > prevScrollPos;
+    window.onscroll = function () {
+      const currentScrollPos = window.pageYOffset;
+      const down = currentScrollPos > prevScrollPos;
 
-			if (down && window.pageYOffset > headerHeight) {
-				gsap.to(headerDiv, { top: -headerHeight });
-			} else if (!down) {
-				gsap.to(headerDiv, { top: 0 });
-			}
+      if (down && window.pageYOffset > headerHeight) {
+        gsap.to(headerDiv, { top: -headerHeight });
+      } else if (!down) {
+        gsap.to(headerDiv, { top: 0 });
+      }
 
-			prevScrollPos = currentScrollPos;
-		};
-	});
+      prevScrollPos = currentScrollPos;
+    };
+  });
 </script>
 
 <header>
-	<nav>
-		<div class="left">
-			<ul>
-				<li><a href="/">🏠</a></li>
-				<li><a href="/blog">✍</a></li>
-			</ul>
-		</div>
-		<div class="middle">
-			<DarkModeToggle />
-		</div>
-		<div class="right" />
-	</nav>
+  <nav>
+    <div class="left">
+      <ul>
+        <li><a href="/">🏠</a></li>
+        <li><a href="/blog">✍</a></li>
+      </ul>
+    </div>
+    <div class="middle">
+      <DarkModeToggle />
+    </div>
+    <div class="right" />
+  </nav>
 </header>
 
 <style lang="scss">
-	header {
-		position: sticky;
-		top: 0px;
-		font-size: 1.5em;
-		z-index: 999;
-	}
+  header {
+    position: sticky;
+    top: 0px;
+    font-size: 1.5em;
+    z-index: 999;
+  }
 
-	nav {
-		width: 100%;
-		height: 2em;
-		background-color: var(--bg);
-		border-bottom: solid 1px var(--border);
-		margin: auto;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		padding: 0;
-		transition: 1s;
+  nav {
+    width: 100%;
+    height: 2em;
+    background-color: var(--bg);
+    border-bottom: solid 1px var(--border);
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0;
+    transition: 1s;
 
-		div {
-			margin-top: 0.5em;
-		}
-	}
+    div {
+      margin-top: 0.5em;
+    }
+  }
 
-	a {
-		text-decoration: none;
-		border: none;
-	}
+  a {
+    text-decoration: none;
+    border: none;
+  }
 
-	ul {
-		display: flex;
-		align-items: center;
-		justify-content: flex-start;
-	}
+  ul {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
 
-	.left,
-	.right,
-	.middle {
-		flex-grow: 1;
-	}
+  .left,
+  .right,
+  .middle {
+    flex-grow: 1;
+  }
 
-	.left {
-		margin-left: 1em;
-	}
+  .left {
+    margin-left: 1em;
+  }
 </style>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,64 @@
+<script>
+  export let data;
+
+  function formatDate(dateString) {
+    return new Intl.DateTimeFormat('en-UK').format(new Date(dateString));
+  }
+</script>
+
+<a href="/blog/tags">Browse tags</a>
+
+<ul>
+  <!-- {#each posts as post}
+		<li>
+			<h2>
+				<a href={post.path}>
+					{post.meta.title}
+				</a>
+			</h2>
+			<div class="details">
+				<div class="date">{formatDate(post.meta.date)}</div>
+				<ul class="tag-list">
+					{#if post.meta.tags}
+						{#each post.meta.tags as tag}
+							<li class="tag">
+								{tag}
+							</li>
+						{/each}
+					{/if}
+				</ul>
+			</div>
+		</li>
+	{/each} -->
+
+  {#each data.summaries as { slug, title }}
+    <li><a href="/blog/{slug}">{title}</a></li>
+  {/each}
+</ul>
+
+<style lang="scss">
+  ul {
+    padding-left: 0;
+    li {
+      list-style-type: none;
+      h2 {
+        font-size: 1.5rem;
+        a {
+          text-decoration: none;
+        }
+      }
+    }
+  }
+
+  .tag-list,
+  .details {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .tag {
+    &:before {
+      content: '🏷️';
+    }
+  }
+</style>
